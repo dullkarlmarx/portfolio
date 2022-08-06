738d4b5ec8e450809d69a38923a6970538f8a221
dullkarlmarx <agustinvazquezrocha33@gmail.com>
2022-08-06 11:16:57 -0300

    da

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 function draw() {
-    const canvas = document.getElementById('canvas');
-  
-      const ctx = canvas.getContext('2d');
+  let canvas = document.getElementById('canvas');
 
-      ctx.fillStyle = 'rgb(200, 0, 0)';
-      ctx.fillRect(10, 10, 50, 50);
+  let ctx = canvas.getContext('2d');
 
-      ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
-      ctx.fillRect(30, 30, 50, 50);
-    }
-  
+  ctx.fillStyle = 'rgb(200, 0, 0)';
+  ctx.fillRect(10, 10, 50, 50);
+
+  ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+  ctx.fillRect(30, 30, 50, 50);
+}
